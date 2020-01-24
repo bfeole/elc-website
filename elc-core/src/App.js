d930c442f84123b08a4f651dabd3d6779d8f125c
Ben Feole <bfeole@gmail.com>
2020-01-24 16:55:25 -0500

    Updated path to services troubleshooting

--- elc-core/src/App.js
+++ elc-core/src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Router history={history}>
         <NavBar />
         <Route exact path='/' component={Home} />
-        <Route exact path='/services' component={Services} />
+        <Route path='/services' component={Services} />
         <Route exact path='/contact' component={Contact} />
       </Router>
     </div>
